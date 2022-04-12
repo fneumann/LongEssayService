0e7f27695463640a01cdea93a1a773bee49e91a4
Fred Neumann <fred.neumann@fau.de>
2022-04-12 02:13:37 +0200

    handle writing steps (continued)

--- src/Writer/Rest.php
+++ src/Writer/Rest.php
@@ -4,10 +4,12 @@ namespace Edutiek\LongEssayService\Writer;
 
 use Edutiek\LongEssayService\Base;
 use Edutiek\LongEssayService\Base\BaseContext;
+use Edutiek\LongEssayService\Data\WritingStep;
 use Edutiek\LongEssayService\Internal\Dependencies;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Slim\Http\StatusCode;
+use DiffMatchPatch\DiffMatchPatch;
 
 class Rest extends Base\BaseRest
 {
@@ -24,7 +26,7 @@ class Rest extends Base\BaseRest
     {
         parent::init($context, $dependencies);
         $this->get('/data', [$this,'getData']);
-
+        $this->put('/steps', [$this,'putSteps']);
     }
 
 
@@ -44,16 +46,83 @@ class Rest extends Base\BaseRest
 
         $task = $this->context->getWritingTask();
 
+        $steps = [];
+        foreach ($this->context->getWritingSteps(50) as $step) {
+            $steps[] = [
+              'timestamp' => $step->getTimestamp(),
+              'content' => $step->getContent(),
+              'is_delta' => $step->isDelta(),
+              'hash_before' => $step->getHashBefore(),
+              'hash_after' => $step->getHashAfter()
+            ];
+        }
+
         $json = [
             'task' => [
                 'title' => $task->getTitle(),
                 'instructions' => $task->getInstructions(),
                 'writer_name' => $task->getWriterName(),
                 'writing_end' => $task->getWritingEnd()
+            ],
+            'essay' => [
+                'content' => $this->context->getWrittenText(),
+                'hash' => $this->context->getWrittenHash(),
+                'steps' => $steps
             ]
         ];
 
         $this->refreshToken();
         return $this->setResponse(StatusCode::HTTP_OK, $json);
+    }
+
+    /**
+     * PUT the settings
+     * @param Request  $request
+     * @param Response $response
+     * @param array $args
+     * @return Response
+     */
+    public function putSteps(Request $request, Response $response, array $args): Response
+    {
+        // common checks and initializations
+        if (!$this->prepare($request, $response, $args)) {
+            return $this->response;
+        }
+
+        $data = $this->request->getParsedBody();
+        if (!isset($data['steps']) || !is_array($data['steps'])) {
+            return $this->setResponse(StatusCode::HTTP_BAD_REQUEST, 'list of steps expected');
+        }
+
+        $dmp = new DiffMatchPatch();
+        $currentText = $this->context->getWrittenText();
+        $currentHash = $this->context->getWrittenHash();
+
+        $steps = [];
+        foreach($data['steps'] as $entry) {
+            $step = new WritingStep(
+                $entry['timestamp'],
+                $entry['content'],
+                $entry['is_delta'],
+                $entry['hash_before'],
+                $entry['hash_after']
+            );
+            $steps[] = $step;
+
+            if ($step->isDelta()) {
+                $patches = $dmp->patch_fromText($step->getContent());
+                $currentText = $dmp->patch_apply($patches, $currentText);
+            }
+            else {
+                $currentText = $step->getContent();
+            }
+            $currentHash = $step->getHashAfter();
+        }
+
+        $this->context->setWrittenText($currentText, $currentHash);
+        $this->context->addWritingSteps($steps);
+
+        $this->refreshToken();
+        return $this->setResponse(StatusCode::HTTP_OK);
     }
 }
