3bdf3a0927bb45ddc16c5ec09b6cc2e54ca375d0
Fred Neumann <fred.neumann@fau.de>
2022-05-20 00:39:45 +0200

    add edit ended

--- src/Writer/Rest.php
+++ src/Writer/Rest.php
@@ -251,6 +251,7 @@ class Rest extends Base\BaseRest
         $this->context->setWrittenEssay($essay
             ->withWrittenText($currentText)
             ->withWrittenHash($currentHash)
+            ->withEditEnded($step ? $step->getTimestamp() : null)
             ->withProcessedText($this->dependencies->html()->processWrittenTextForDisplay($currentText))
         );
     }
